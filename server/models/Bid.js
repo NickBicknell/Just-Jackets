f6e3a6e24a0c738bf54e689a9e8a91073e795e29
Antonios Zikos <antonioszikos@Antonioss-MBP.attlocal.net>
2023-11-02 19:59:06 -0500

    working on bid and user schemas

---
+++ server/models/Bid.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require('mongoose');
+const productSchema = require('./Product')
+const dateFormat = require('../utils/dateFormat');
+const 
+
+const bidSchema = new Schema({
+  bidId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  amount: {
+    type: INT,
+    required: true,
+    trim: true,
+  },
+  size: {
+    type: String,
+    default: "M",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp),
+  },
+  productId: [productSchema],
+  
+});
+
+const Bid = model('Bid', bidSchema);
+
+module.exports = Bid;
--- server/models/User.js
+++ server/models/User.js
@@ -19,12 +19,13 @@ const userSchema = new Schema({
     required: true,
     minlength: 5,
   },
-  thoughts: [
+  address:
     {
-      type: Schema.Types.ObjectId,
-      ref: 'Thought',
+      type: String,
+      required: true,
     },
-  ],
+    
+
 });
 
 userSchema.pre('save', async function (next) {
