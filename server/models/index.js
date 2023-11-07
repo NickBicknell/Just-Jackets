a10a3cef4eb3d9b69c4021b9c3d339a651612881
Antonios Zikos <antonioszikos@Antonioss-MacBook-Pro.local>
2023-11-06 19:09:18 -0500

    Model Changes

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,5 @@
 const User = require('./User');
-const Thought = require('./Thought');
+const Bid = require('./Bid');
+const Product = require('./Product');
 
-module.exports = { User, Thought };
+module.exports = { User, Product, Bid };
