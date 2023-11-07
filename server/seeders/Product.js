fa90fd2c8c54c7b4382566c7c0943f7b377bf759
Antonios Zikos <antonioszikos@Antonioss-MacBook-Pro.local>
2023-11-06 19:48:18 -0500

    seed data

---
+++ server/seeders/Product.js
@@ -0,0 +1,18 @@
+const seedData = [
+    {
+      name: 'Jacket 1',
+      brand: 'Brand A',
+      size: 'M',
+    },
+    {
+      name: 'Jacket 2',
+      brand: 'Brand B',
+      size: 'L',
+    },
+    {
+      name: 'Jacket 3',
+      brand: 'Brand C',
+      size: 'S',
+    },
+  ];
+  
